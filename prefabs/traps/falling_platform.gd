47ffd550d8b6aa7ae1dd54cfa4b7f5d9413ccb8c
David <david.gawin@gmail.com>
2025-09-19 16:27:56 +0200

    Redid collissions & Fallingplatform

--- prefabs/traps/falling_platform.gd
+++ prefabs/traps/falling_platform.gd
@@ -36,5 +36,5 @@ func _initiate_platform_fall():
 
 func _on_player_detection_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
-		touched = true
-		pass
+		if body.velocity.y <= 0:
+			touched = true
