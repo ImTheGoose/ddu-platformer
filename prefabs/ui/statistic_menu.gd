2a2f5f7d42fe836e403de8f1b9058e3ae4207d81
David <david.gawin@gmail.com>
2025-10-09 13:23:08 +0200

    Lots of icons (for statistics)

--- prefabs/ui/statistic_menu.gd
+++ prefabs/ui/statistic_menu.gd
@@ -1,26 +1,26 @@
 extends GameMenu
 
-const bbcode_string = "[img]res://assets/icons/time (16x16).png[/img]Time played - {time_played}
-[img]res://assets/icons/heart (16x16).png[/img]Time alive - {time_alive}
-[img]res://assets/icons/medal (16x16).png[/img]Highscore - {time_highscore}
-Most apples in a round - {apple_highscore}
+const bbcode_string = "[img]res://assets/icons/time (16x16).png[/img] Time played - {time_played}
+[img]res://assets/icons/heart_red (16x16).png[/img] Time alive - {time_alive}
+[img]res://assets/icons/star (16x16).png[/img] Highscore - {time_highscore}
+[img]res://assets/icons/star_red (16x16).png[/img] Most apples in a round - {apple_highscore}
 [img]res://assets/pixel_adventure_assets/Items/Fruits/Apple_16x16.png[/img] Apples collected - {total_apples_collected}
 
-Total Jumps - {total_jumps}
-Ground jumps - {ground_jumps}
-Wall jumps - {wall_jumps}
-Double jumps - {double_jumps}
-Trampoline jumps - {trampoline_jumps}
-Enemy jums - {enemy_jumps}
+[img]res://assets/icons/lightning (16x16).png[/img] Total Jumps - {total_jumps}
+[img]res://assets/icons/ground (16x16).png[/img] Ground jumps - {ground_jumps}
+[img]res://assets/icons/wall (16x16).png[/img] Wall jumps - {wall_jumps}
+[img]res://assets/icons/double_jump (16x16).png[/img] Double jumps - {double_jumps}
+[img]res://assets/icons/trampoline (16x16).png[/img] Trampoline jumps - {trampoline_jumps}
+[img]res://assets/icons/dual_enemy (16x16).png[/img] Enemy jumps - {enemy_jumps}
 
-Total Deaths - {total_deaths}
+[img]res://assets/icons/bone_skull (16x16).png[/img] Total Deaths - {total_deaths}
 [img]res://assets/pixel_adventure_assets/Enemies/Mushroom/Icon (16x16).png[/img] Mushrooms deaths - {mushroom_deaths}
 [img]res://assets/pixel_adventure_assets/Enemies/Trunk/Trunk (16x16).png[/img] Trunks deaths - {trunk_deaths}
 [img]res://assets/pixel_adventure_assets/Traps/Spikes/Icon (16x16).png[/img] Spike deaths - {spike_deaths}
 [img]res://assets/pixel_adventure_assets/Traps/Fire/Icon (16x16).png[/img] Fire deaths - {fire_deaths}
 [img]res://assets/pixel_adventure_assets/Other/Dust Particle.png[/img] Cloud deaths - {cloud_deaths}
 
-Total Killed - {total_killed}
+[img]res://assets/icons/sword (16x16).png[/img] Total Killed - {total_killed}
 [img]res://assets/pixel_adventure_assets/Enemies/Mushroom/Icon (16x16).png[/img] Mushrooms killed - {mushroom_killed}
 [img]res://assets/pixel_adventure_assets/Enemies/Trunk/Trunk (16x16).png[/img] Trunks killed - {trunk_killed}"
 
