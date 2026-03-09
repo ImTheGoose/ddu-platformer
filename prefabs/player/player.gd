c787710718db6aa4df2f46846e2e9b952681129d
David <david.gawin@gmail.com>
2026-03-09 20:32:30 +0100

    playerscript_rewrite

--- prefabs/player/player.gd
+++ prefabs/player/player.gd
@@ -1,166 +1,119 @@
 extends CharacterBody2D
 
-@onready var anim :AnimatedSprite2D = $AnimatedSprite2D
+class_name Player
+
 @export var jump_strength :int = 1250
-@export var speed_per_second :int = 3000
+@export var accelleration_per_second :int = 3000
 @export var max_speed :int = 450
 @export var jump_buffer_time :float = 0.1
 @export var wall_gravity_scale :float = 0.15
 
-@onready var audio_files :Dictionary[String, AudioStreamMP3]= {
-	"running" : preload("uid://cap73awyf8ake"),
-	"jump" : preload("uid://we1luimcp6fb"),
-	"die" : preload("uid://dafct6iqadbur"),
-}
+@onready var collission_shape_2d :CollisionShape2D = %CollisionShape2D
 
-@onready var dust_particles :GPUParticles2D = $dust_particles
-@onready var jump_particles :GPUParticles2D = $jump_particles
-@onready var death_particles :GPUParticles2D = $die_particles
-@onready var audio_stream :AudioStreamPlayer = $AudioStreamPlayer
-
-
-var dead :bool = false #TEMPOARY
 var double_jumped :bool = false
 var air_time :float = 0
 
+const INPUT_MAP :Dictionary[String, String] = { ## Skal ændres til globalt inputmap
+	"move_left" : "move_left",
+	"move_right" : "move_right",
+	"jump_action" : "jump"
+}
+
+var state :PlayerState = PlayerState.IDLE
+
+enum PlayerState {
+	IDLE,
+	RUNNING,
+	ON_WALL,
+	IN_AIR,
+	DEAD,
+}
 
 func _physics_process(delta: float) -> void:
-	if dead:
-		var col :CollisionShape2D = $CollisionShape2D
-		col.disabled = true
-		_limit_horizontal_velocity(max_speed * 4)
-		_reduce_horizontal_velocity(delta, speed_per_second / 20)
-		_apply_gravity(delta, 0.65)
-		move_and_slide()
-		return
+	if state == PlayerState.DEAD:
+		_handle_dead_player(delta)
+	else:
+		_handle_alive_player(delta)
+	move_and_slide()
+	print(state)
+
+func _handle_dead_player(delta: float) -> void:
+	velocity.x = clamp(velocity.x, -max_speed * 4, max_speed * 4)
+	velocity += get_gravity() * delta * 0.65
+	collission_shape_2d.disabled = true
+	_reduce_horizontal_velocity(delta, accelleration_per_second / 20)
+
+func _handle_alive_player(delta: float) -> void:
+	var move_axis :float = Input.get_axis(INPUT_MAP["move_left"], INPUT_MAP["move_right"])
 	
-	var move_axis :float = Input.get_axis("move_left", "move_right")
-	_limit_horizontal_velocity(max_speed)
+	velocity.x = clamp(velocity.x, -max_speed, max_speed)
+	velocity.x += move_axis * accelleration_per_second * delta
+	
+	
 	if move_axis == 0:
-		_reduce_horizontal_velocity(delta, speed_per_second)
+		_reduce_horizontal_velocity(delta, accelleration_per_second)
 	
-	if is_on_floor() or is_on_wall():
+	if is_on_floor():
+		if move_axis == 0:
+			state = PlayerState.IDLE
+		else:
+			state = PlayerState.RUNNING
+	
+	elif is_on_wall():
+		state = PlayerState.ON_WALL
+		
+	else:
+		state = PlayerState.IN_AIR
+	
+	if is_player_grounded():
 		double_jumped = false
 		air_time = 0
 	else:
 		air_time += delta
-		
-	#Has to be after to ensure air_time is igonered if player jumps while on floor.
-	if Input.is_action_just_pressed("jump"):
-		_attempt_jump()
 
-	velocity.x += move_axis * speed_per_second * delta
-
-	if is_on_wall_only() && velocity.y > 0:
-		anim.play("Wall_Jump")
-		_apply_gravity(delta, wall_gravity_scale)
+	if Input.is_action_just_pressed(INPUT_MAP["jump_action"]):
+		_handle_jump()
+	
+	if state == PlayerState.ON_WALL && velocity.y > 0:
+		velocity += get_gravity() * delta * wall_gravity_scale
 	else:
-		_apply_gravity(delta)
-	
-
-	move_and_slide()
-	_update_anim(move_axis)
-
-func _attempt_jump() -> void:
-	if is_on_wall_only():
-		velocity.y = -jump_strength * 0.85
-		if anim.flip_h:
-			velocity.x = max_speed
-		else:
-			velocity.x = -max_speed
-		air_time = jump_buffer_time
-		anim.flip_h = !anim.flip_h
-		jump_particles.restart(false)
-		audio_stream.stream = audio_files["jump"]
-		audio_stream.volume_db = -20
-		audio_stream.pitch_scale = randf_range(0.8, 1.1)
-		audio_stream.play()
-		StatisticManager.add_value("wall_jump", 1)
-	
-	elif is_on_floor() or air_time < jump_buffer_time:
-		jump_particles.restart(false)
-		air_time = jump_buffer_time
-		velocity.y = -jump_strength
-		audio_stream.stream = audio_files["jump"]
-		audio_stream.volume_db = -20
-		audio_stream.pitch_scale = randf_range(0.8, 1.1)
-		audio_stream.play()
-		StatisticManager.add_value("ground_jump", 1)
-
-	elif !double_jumped:
-		double_jumped = true
-		velocity.y = -jump_strength * 0.85
-		anim.play("Double_Jump")
-		jump_particles.restart(false)
-		audio_stream.stream = audio_files["jump"]
-		audio_stream.volume_db = -20
-		audio_stream.pitch_scale = randf_range(0.8, 1.1)
-		audio_stream.play()
-		StatisticManager.add_value("double_jump", 1)
-
-func _apply_gravity(delta: float, gravity_scale: float = 1) -> void:
-	velocity += get_gravity() * delta * gravity_scale
-	
-
-func _limit_horizontal_velocity(max_vel: int) -> void:
-	if velocity.x > 20:
-		if velocity.x > max_vel:
-			velocity.x = max_vel
-	elif velocity.x < -20:
-		if velocity.x < -max_vel:
-			velocity.x = -max_vel
-	else:
-		velocity.x = 0
+		velocity += get_gravity() * delta * 1
 
 func _reduce_horizontal_velocity(delta: float, amount_per_second: float) -> void:
 	if velocity.x < -20:
 		velocity.x += amount_per_second * delta
 	elif velocity.x > 20:
 		velocity.x -= amount_per_second * delta
+	else:
+		velocity.x = 0
 
-
-func _die() -> void: #TEMPOARY
-	print("player dying")
-	dead = true
-	GameManager.player_died()
-	death_particles.restart()
-	anim.play("Die")
-	audio_stream.stream = audio_files["die"]
-	audio_stream.volume_db = -8
-	audio_stream.pitch_scale = randf_range(0.9, 1.1)
-	audio_stream.play()
+func is_player_grounded() -> bool:
+	return state == PlayerState.ON_WALL or state == PlayerState.IDLE or state == PlayerState.RUNNING
 
 func hit(vec: Vector2) -> void:
 	velocity = vec * max_speed * 1.5
-	_die()
+	#_die()
 
-func _update_anim(move_axis: float) -> void:
-	if dead:
-		return
-		
-	if move_axis > 0:
-		anim.flip_h = false
-	if move_axis < 0:
-		anim.flip_h = true
-
-	if is_on_floor():
-		if move_axis == 0:
-			anim.play("Idle")
-			dust_particles.emitting = false
-			audio_stream.stop()
+func _handle_jump() -> void:
+	if state == PlayerState.ON_WALL:
+		velocity.y = -jump_strength * 0.85
+		if get_wall_normal().normalized().x > 0:
+			velocity.x = max_speed
 		else:
-			anim.play("Run")
-			dust_particles.emitting = true
-			if audio_stream.stream != audio_files["running"] or !audio_stream.playing:
-				audio_stream.stream = audio_files["running"]
-				audio_stream.volume_db = -14
-				audio_stream.pitch_scale = randf_range(0.9, 1.1)
-				audio_stream.play()
-	elif !is_on_wall_only():
-		if velocity.y < 0 && !double_jumped:
-			dust_particles.emitting = false
-			anim.play("Jump")
-
-		if velocity.y > 0:
-			anim.play("Fall")
-			dust_particles.emitting = false
+			velocity.x = -max_speed
+		
+		air_time = jump_buffer_time
+		StatisticManager.add_value("wall_jump", 1)
+		return
+	
+	if state == PlayerState.IDLE or state == PlayerState.RUNNING or air_time < jump_buffer_time:
+		air_time = jump_buffer_time
+		velocity.y = -jump_strength
+		StatisticManager.add_value("ground_jump", 1)
+		return
+	
+	if !double_jumped:
+		double_jumped = true
+		velocity.y = -jump_strength * 0.85
+		StatisticManager.add_value("double_jump", 1)
+	
--- prefabs/player/player_skin.gd
+++ prefabs/player/player_skin.gd
@@ -1,5 +1,7 @@
 extends AnimatedSprite2D
 
+@export var player :Player
+
 @onready var skin_sprites :Dictionary[String, SpriteFrames]= {
 	"Osvald": preload("uid://b4t3asxpw884d"),
 	"Tiki": preload("uid://copebinntgo64"),
@@ -7,7 +9,30 @@ extends AnimatedSprite2D
 	"Edward": preload("uid://dvv7gt1jhi3uo"),
 }
 
-
 func _ready() -> void:
 	var skin_name :String = DataManager.get_value("selected_skin")
 	sprite_frames = skin_sprites[skin_name]
+
+func _process(delta: float) -> void:
+	if !player:
+		return
+	
+	if player.velocity.x > 0:
+		flip_h = false
+	elif player.velocity.x < 0:
+		flip_h = true
+	
+	if player.state == Player.PlayerState.IDLE:
+		play("Idle")
+	elif player.state == Player.PlayerState.RUNNING:
+		play("Run")
+	elif player.state == Player.PlayerState.ON_WALL:
+		play("Wall_Jump")
+	elif player.state == Player.PlayerState.IN_AIR:
+		if player.velocity.y > 0:
+			play("Fall")
+		elif player.double_jumped:
+			play("Double_Jump")
+		else:
+			play("Jump")
+		
