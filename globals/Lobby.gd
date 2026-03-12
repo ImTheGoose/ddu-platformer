be8d5397e602ab2e1c427f1c8c1225543a2c0238
David <david.gawin@gmail.com>
2026-03-12 12:53:43 +0100

    Code refactor and steam offline checks

--- globals/Lobby.gd
+++ globals/Lobby.gd
@@ -17,57 +17,18 @@ func _ready() -> void:
 	multiplayer.connection_failed.connect(_on_connection_failed)
 	multiplayer.server_disconnected.connect(_on_server_disconnected)
 	multiplayer.peer_connected.connect(_on_peer_connected)
-	Steam.join_requested.connect(_on_join_requested)
+	Steam.join_requested.connect(_on_steam_join_requested)
 	Steam.lobby_created.connect(_on_steam_lobby_created)
 	Steam.lobby_joined.connect(_on_steam_lobby_joined)
 	Steam.lobby_chat_update.connect(_on_lobby_chat_update)
 	close_connection()
 
+#region Lan Lobby Hadling
+
 func _on_server_disconnected() -> void:
 	Alerts.push_error("Disconnected", "The lobby host has left.")
 	close_connection()
 	MenuHandler.change_menu("main_menu")
-
-func _on_join_requested(lobby_id: int, steam_id: int) -> void:
-	Lobby.join_steam_lobby(lobby_id)
-
-func _on_steam_lobby_joined(lobby: int, permission: int, locked: bool, response: int) -> void:
-	if response == Steam.CHAT_ROOM_ENTER_RESPONSE_SUCCESS:
-		if multiplayer.multiplayer_peer && multiplayer.is_server():
-			return
-		STEAM_LOBBY_ID = lobby
-		STEAM_PEER = SteamMultiplayerPeer.new()
-		STEAM_PEER.server_relay = true
-		STEAM_PEER.create_client(Steam.getLobbyOwner(lobby))
-		multiplayer.multiplayer_peer = STEAM_PEER
-		add_player_info(multiplayer.multiplayer_peer.get_unique_id())
-		_on_connected_to_server()
-	else:
-		_on_connection_failed()
-
-func _on_lobby_chat_update(this_lobby_id: int, change_id: int, making_change_id: int, chat_state: int) -> void:
-	# Get the user who has made the lobby change
-	var changer_name: String = Steam.getFriendPersonaName(change_id)
-
-	# If a player has joined the lobby
-	if chat_state == Steam.CHAT_MEMBER_STATE_CHANGE_ENTERED:
-		print("%s has joined the lobby." % changer_name)
-
-	# Else if a player has left the lobby
-	elif chat_state == Steam.CHAT_MEMBER_STATE_CHANGE_LEFT:
-		print("%s has left the lobby." % changer_name)
-
-	# Else if a player has been kicked
-	elif chat_state == Steam.CHAT_MEMBER_STATE_CHANGE_KICKED:
-		print("%s has been kicked from the lobby." % changer_name)
-
-	# Else if a player has been banned
-	elif chat_state == Steam.CHAT_MEMBER_STATE_CHANGE_BANNED:
-		print("%s has been banned from the lobby." % changer_name)
-
-	# Else there was some unknown change
-	else:
-		print("%s did... something." % changer_name)
 
 func _on_connected_to_server() -> void:
 	MenuHandler.change_menu("multiplayer_lobby_menu")
@@ -98,26 +59,44 @@ func join_lan_server(ip: String, port: int = DEFAULT_LAN_PORT) -> void:
 		close_connection()
 		return
 
-func is_lobby_lan() -> bool:	
-	if multiplayer.multiplayer_peer is SteamMultiplayerPeer:
-		return false
+#endregion
+
+#region Steam Lobby Handling
+
+func _on_lobby_chat_update(this_lobby_id: int, change_id: int, making_change_id: int, chat_state: int) -> void:
+	# Get the user who has made the lobby change
+	var changer_name: String = Steam.getFriendPersonaName(change_id)
+
+	# If a player has joined the lobby
+	if chat_state == Steam.CHAT_MEMBER_STATE_CHANGE_ENTERED:
+		print("%s has joined the lobby." % changer_name)
+
+	# Else if a player has left the lobby
+	elif chat_state == Steam.CHAT_MEMBER_STATE_CHANGE_LEFT:
+		print("%s has left the lobby." % changer_name)
+
+	# Else if a player has been kicked
+	elif chat_state == Steam.CHAT_MEMBER_STATE_CHANGE_KICKED:
+		print("%s has been kicked from the lobby." % changer_name)
+
+	# Else if a player has been banned
+	elif chat_state == Steam.CHAT_MEMBER_STATE_CHANGE_BANNED:
+		print("%s has been banned from the lobby." % changer_name)
+
+	# Else there was some unknown change
 	else:
-		return true
+		print("%s did... something." % changer_name)
+
+func _on_steam_join_requested(lobby_id: int, steam_id: int) -> void:
+	Lobby.join_steam_lobby(lobby_id)
 
 func create_steam_lobby() -> void:
 	MenuHandler.change_menu("multiplayer_status_menu")
-	Steam.createLobby(Steam.LobbyType.LOBBY_TYPE_PUBLIC, 4)
-
-func join_steam_lobby(lobby_id: int) -> void:
-	MenuHandler.change_menu("multiplayer_status_menu")
-	Steam.joinLobby(lobby_id)
-
-func kick_peer(peer_id: int) -> void:
-	multiplayer.multiplayer_peer.disconnect_peer(peer_id)
-
-func _on_peer_connected(peer_id: int) -> void:
-	add_player_info(peer_id)
-
+	if Steam.loggedOn():
+		Steam.createLobby(Steam.LobbyType.LOBBY_TYPE_PUBLIC, 4)
+	else:
+		Lobby.connection_error.emit("No connection to steam servers.")
+		
 func _on_steam_lobby_created(result: int, id: int) -> void:
 	if result == Steam.Result.RESULT_OK:
 		STEAM_LOBBY_ID = id
@@ -132,6 +111,43 @@ func _on_steam_lobby_created(result: int, id: int) -> void:
 		MenuHandler.change_menu("multiplayer_lobby_menu")
 		Alerts.push_success("Lobby Created", "Lobby was successfully created.")
 
+func join_steam_lobby(lobby_id: int) -> void:
+	MenuHandler.change_menu("multiplayer_status_menu")
+	if Steam.loggedOn():
+		Steam.joinLobby(lobby_id)
+	else:
+		Lobby.connection_error.emit("No connection to steam servers.")
+
+func _on_steam_lobby_joined(lobby: int, permission: int, locked: bool, response: int) -> void:
+	if response == Steam.CHAT_ROOM_ENTER_RESPONSE_SUCCESS:
+		if multiplayer.multiplayer_peer && multiplayer.is_server():
+			return
+		STEAM_LOBBY_ID = lobby
+		STEAM_PEER = SteamMultiplayerPeer.new()
+		STEAM_PEER.server_relay = true
+		STEAM_PEER.create_client(Steam.getLobbyOwner(lobby))
+		multiplayer.multiplayer_peer = STEAM_PEER
+		add_player_info(multiplayer.multiplayer_peer.get_unique_id())
+		_on_connected_to_server()
+	else:
+		_on_connection_failed()
+
+#endregion
+
+#region Lobby helper functions
+
+func is_lobby_lan() -> bool:	
+	if multiplayer.multiplayer_peer is SteamMultiplayerPeer:
+		return false
+	else:
+		return true
+
+func kick_peer(peer_id: int) -> void:
+	multiplayer.multiplayer_peer.disconnect_peer(peer_id)
+
+func _on_peer_connected(peer_id: int) -> void:
+	add_player_info(peer_id)
+
 func get_peer_id_from_steam_id(steam_id: int) -> int:
 	if multiplayer.multiplayer_peer is SteamMultiplayerPeer:
 		return multiplayer.multiplayer_peer.get_peer_id_for_steam_id(steam_id)
@@ -141,7 +157,6 @@ func get_steam_id_from_peer_id(peer_id: int) -> int:
 	if multiplayer.multiplayer_peer is SteamMultiplayerPeer:
 		return multiplayer.multiplayer_peer.get_steam_id_for_peer_id(peer_id)
 	return peer_id
-	
 
 func add_player_info(peer_id: int) -> void:
 	created_player_infos.set(peer_id, PlayerInfo.new(peer_id))
@@ -164,3 +179,5 @@ func close_connection() -> void:
 	if STEAM_LOBBY_ID > 0:
 		Steam.leaveLobby(STEAM_LOBBY_ID)
 		STEAM_LOBBY_ID = 0
+
+#endregion
--- prefabs/menus/start_game_menu/select_play_menu.gd
+++ prefabs/menus/start_game_menu/select_play_menu.gd
@@ -1,6 +1,19 @@
 extends GameMenu
 
+@onready var online_multiplayer_button :Button = %online_multiplayer
 
+func _ready() -> void:
+	super()
+	online_multiplayer_button.disabled = !Steam.loggedOn()
+	Steam.steam_server_connected.connect(_on_steam_server_connected)
+	Steam.steam_server_disconnected.connect(_on_steam_server_disconnected)
+
+func _on_steam_server_disconnected(result: int) -> void:
+	online_multiplayer_button.disabled = !Steam.loggedOn()
+
+func _on_steam_server_connected() -> void:
+	online_multiplayer_button.disabled = !Steam.loggedOn()
+ 
 func _on_singleplayer_pressed() -> void:
 	MenuHandler.change_menu("start_game_menu")
 	pass # Replace with function body.
