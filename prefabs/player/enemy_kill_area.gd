d32dcca6827a517044e8c9798b02e71104addfad
David <david.gawin@gmail.com>
2026-04-03 15:16:01 +0200

    Removed Old Enemy Code

--- prefabs/player/enemy_kill_area.gd
+++ prefabs/player/enemy_kill_area.gd
@@ -6,12 +6,13 @@ func _init() -> void:
 
 func _on_area_entered(area: Area2D) -> void:
 	if is_multiplayer_authority():
-		if area.get_parent() is PathfindingEnemy:
-			if get_parent().is_on_floor():
-				return
-			area.get_parent().rpc("die")
-			Stats.add_kill_to_recording(area.get_parent().enemy_type)
-			
-			get_parent().velocity.y = -1000
-			get_parent().double_jumped = false
-			Stats.add_recording_value(Stats.StatType.JUMPS_KILL, 1)
+		return
+		#if area.get_parent() is PathfindingEnemy:
+			#if get_parent().is_on_floor():
+				#return
+			#area.get_parent().rpc("die")
+			#Stats.add_kill_to_recording(area.get_parent().enemy_type)
+			#
+			#get_parent().velocity.y = -1000
+			#get_parent().double_jumped = false
+			#Stats.add_recording_value(Stats.StatType.JUMPS_KILL, 1)
